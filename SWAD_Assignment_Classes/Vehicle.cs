93c8bb2487e5d7964da91308585ce75dce25195c
JinXuan03 <148025005+JinXuan03@users.noreply.github.com>
2024-08-11 03:22:49 +0800

    Update Vehicle.cs

--- SWAD_Assignment_Classes/Vehicle.cs
+++ SWAD_Assignment_Classes/Vehicle.cs
@@ -1,31 +1,19 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace SWAD_Assignment_Classes
 {
     internal class Vehicle
     {
         public int Id { get; set; }
-
         public string Make { get; set; }
-
         public string Model { get; set; }
-
         public string Year { get; set; }
-
         public int Mileage { get; set; }
-
         public string LicensePlateNumber { get; set; }
-
         public int RentalRate { get; set; }
-
         public bool IsHidden { get; set; }
-
         public List<DateTime> ListOfAvailableBookingDateTimes { get; set; }
-
         public List<Booking> BookingList { get; set; }
 
         public Vehicle(int id, string make, string model, string year, int mileage, string licensePlateNumber, int rentalRate, bool isHidden, List<DateTime> listOfAvailableBookingDateTimes, List<Booking> bookingList)
@@ -42,39 +30,33 @@ namespace SWAD_Assignment_Classes
             BookingList = bookingList;
         }
 
-        public List<DateTime> getAvailableBookingDateTimes()
+        public List<DateTime> GetAvailableBookingDateTimes()
         {
             return ListOfAvailableBookingDateTimes;
         }
 
-        public bool checkIfDateTimesAvailable(DateTime startDateTime, DateTime endDateTime)
+        public bool CheckIfDateTimesAvailable(DateTime startDateTime, DateTime endDateTime)
         {
-            if (ListOfAvailableBookingDateTimes.Contains(startDateTime) && ListOfAvailableBookingDateTimes.Contains(endDateTime))
-            {
-                return true;
-            } else
-            {
-                return false;
-            }
+            return ListOfAvailableBookingDateTimes.Contains(startDateTime) && ListOfAvailableBookingDateTimes.Contains(endDateTime);
         }
 
-        public void addBooking(Booking booking)
+        public void AddBooking(Booking booking)
         {
             BookingList.Add(booking);
         }
 
-        public void updateBookingDateTimes(DateTime startDateTime, DateTime endDateTime)
+        public void UpdateBookingDateTimes(DateTime startDateTime, DateTime endDateTime)
         {
             ListOfAvailableBookingDateTimes.Remove(startDateTime);
             ListOfAvailableBookingDateTimes.Remove(endDateTime);
         }
 
-        public void deleteBooking(Booking booking)
+        public void DeleteBooking(Booking booking)
         {
             BookingList.Remove(booking);
         }
 
-        public void releaseBookingDateTimes(DateTime startDateTime, DateTime endDateTime)
+        public void ReleaseBookingDateTimes(DateTime startDateTime, DateTime endDateTime)
         {
             ListOfAvailableBookingDateTimes.Add(startDateTime);
             ListOfAvailableBookingDateTimes.Add(endDateTime);
